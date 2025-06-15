d3bfa81c7776ac93ad9a75167883542656f33c2f
Xawas <karol.kem@proton.me>
2025-06-15 18:49:42 +0200

    working login and register

--- TestBlazor/Program.cs
+++ TestBlazor/Program.cs
@@ -5,7 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("http://localhost:5220")
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ TestBlazor/RegisterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TestBlazor
+{
+    public class RegisterDto
+    {
+        public string email {  get; set; }
+        public string login { get; set; }
+        public string password { get; set; }
+    }
+}
