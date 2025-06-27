af191772b366aee5d512a1f0ba7203b415e29fea
Xawas <karol.kem@proton.me>
2025-06-27 22:50:12 +0200

    add test transaction

--- TestBlazor/obj/Debug/net8.0/TestBlazor.AssemblyInfo.cs
+++ TestBlazor/obj/Debug/net8.0/TestBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a2d149d5ffb1c79479a8408b3a2046597141659")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38469bbfeec272f9eb3619c8fc2e4875f129fbcf")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
